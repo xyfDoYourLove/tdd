54119cfebb5a0976137609786f311dee40f7b807
DoYourLove <xyf15620168540@163.com>
2020-08-03 18:03:59 +0800

    [XiaoYiFan] feat:rafactoring

--- src/main/java/Mommifier.java
+++ src/main/java/Mommifier.java
@@ -5,7 +5,6 @@ import java.util.Map;
 public class Mommifier {
 
     public static Map<Character, Character> vowels = new HashMap<>();
-
     static {
         vowels.put('a', 'a');
         vowels.put('e', 'e');
@@ -16,36 +15,37 @@ public class Mommifier {
 
     public String dealWithVowels(String args) {
         char[] chars = args.toCharArray();
-        double vowelCount = 0.0;
-        int continuousVowel = 0;
-
         StringBuffer result = new StringBuffer();
+        double vowelCount = 0.0;
+        int lastVowel = 0;
 
         for (int i = 0; i < chars.length; i++) {
-
             if (vowels.get(chars[i]) != null) {
                 vowelCount++;
-                continuousVowel++;
-                if (i == chars.length-1 && continuousVowel > 1) {
+                lastVowel++;
+                if (i == chars.length-1 && lastVowel > 1) {
                     result.append(chars[i]);
                     result.append("mommy");
                     break;
                 }
             }else {
-                if (continuousVowel > 1) {
-                    result.append("mommy");
-                }
-                continuousVowel = 0;
+                result = insertStringAfterVowels(result, lastVowel);
+                lastVowel = 0;
             }
-
             result.append(chars[i]);
         }
 
         if (vowelCount/(double)chars.length > 0.3) {
             return result.toString();
         }
-
         return args;
+    }
+
+    public StringBuffer insertStringAfterVowels(StringBuffer result, int lastVowel) {
+        if (lastVowel > 1) {
+            result.append("mommy");
+        }
+        return result;
     }
 
 
